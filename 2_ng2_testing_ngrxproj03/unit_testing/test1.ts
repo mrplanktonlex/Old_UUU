f1c91deea2bc09d3c0bb6fbd25184bb6337980be
planktonlex55 <planktonlex55@gmail.com>
2017-06-21 11:59:39 +0530

    Update test1.ts

--- 2_ng2_testing_ngrxproj03/unit_testing/test1.ts
+++ 2_ng2_testing_ngrxproj03/unit_testing/test1.ts
@@ -1,1 +1,19 @@
 
+import {Component} from '@angular/core';
+
+@Component({
+})
+
+export default class VoterComponent{
+vote:number = 0;
+
+  plusvote(){
+    this.vote++ ;
+    return this.vote;
+  }
+
+  minusvote(){
+    this.vote--;
+    return this.vote;
+  }
+}
